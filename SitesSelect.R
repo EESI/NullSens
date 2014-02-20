ecc0af61a1dfe9a04b10c724121548f21f8a9962
groovyjawn <sessinger@gmail.com>
2014-02-19 20:16:58 -0500

    NullSens separated into sub-functions

--- SitesSelect.R
+++ SitesSelect.R
@@ -1,19 +1,20 @@
-SitesSelect <- function() {
+sitesSelect <- function(CDM,X,n,p,q) {
 
+SitesSel <- vector('list',p) # Stores Sites Selected for each Species
 
-# Site Selection and SitesSelion
+# Site Selection
 for (i in 1:p){ # For each species
 	posAb <- which(CDM[,i] > 0) # Choose sites with positive abundance for theshold
 	remove <- NA # initialize sites for removal
-	centroid <- c(rep(0,q)) 
-	thresh <- c(rep(0,q))
+	centroid <- c(rep(0,q)) # Median of each gradient
+	thresh <- c(rep(0,q)) # Threshold for site removal
 	
 	for (grad in 2:q){ # For each abiotic factor
-		centroid[grad] <- quantile(X[posAb,grad],0.5) # Centroid of positive abundance
-		distance_pos <- as.matrix(dist(c(centroid[grad],X[posAb,grad]))) # distance of positive abundances to centroid
+		centroid[grad] <- quantile(X[posAb,grad],0.5) # Centroid (Median) of positive abundance
+		distance_pos <- as.matrix(dist(c(centroid[grad],X[posAb,grad]))) # distances of positive abundances to centroid
 		thresh[grad] <- mean(distance_pos[-1,1]) + sd(distance_pos[-1,1]) # threshold for site selection
 	
-		distance_all <- as.matrix(dist(c(centroid[grad],X[,grad]))) # Distance of all sites (positive and zero) to centroid
+		distance_all <- as.matrix(dist(c(centroid[grad],X[,grad]))) # distances of all sites (positive and zero) to centroid
 		remove_temp <- which(distance_all[-1,1] > thresh[grad]) # remove sites beyond threshold from further analysis
 		remove <- c(remove,remove_temp) # keep a list of all sites removed for each species
 	}
@@ -23,4 +24,6 @@ for (i in 1:p){ # For each species
 	if (length(keep) > 3){ # remove species with < 4 selected sites from analysis
 		SitesSel[[i]][keep] = TRUE	
 	}
+}
+return(SitesSel)
 }
