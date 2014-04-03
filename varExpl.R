c876125dcdd95d43a0e4e966a2b74c9e385f7e52
groovyjawn <sessinger@gmail.com>
2014-04-02 23:19:29 -0400

    debug CDM_summary'

--- varExpl.R
+++ varExpl.R
@@ -7,21 +7,23 @@ for (i in 1:p){
 }
 
 if (PRSqYres > 0.05){ # If no significant covariation found
-	INDsummary <- cbind(Adj_R2,matrix(0,p,1),1-Adj_R2) # Abiotic, Biotic, Unexplained, per species
-	COMsummary <- cbind(Avg_Adj_R2,0,1-Avg_Adj_R2) # Abiotic, Biotic, Unexplained, community
+	env = Adj_R2; bio = matrix(0,p,1); unx = 1-Adj_R2
+	ENV = Avg_Adj_R2; BIO = 0; UNX = 1-Avg_Adj_R2
+	IND_summary <- cbind(Adj_R2,matrix(0,p,1),1-Adj_R2) # Abiotic, Biotic, Unexplained, per species
+	COM_summary <- cbind(Avg_Adj_R2,0,1-Avg_Adj_R2) # Abiotic, Biotic, Unexplained, community
 
-} else{ # If significant covariation found
+} else { # If significant covariation found
 	# Individual species variation partitioning
-	sumENV <- Adj_R2
-	unexp <- 1-sumENV # Abiotic variation explained
+	env <- Adj_R2
+	unx <- 1-env # Abiotic variation explained
 	cr <- rbind(CR,matrix(0,1,p))
 	BR <- cr + t(cr)
 	BR2 <- BR^2 # Squared correlation matrix of pairwise species
 	pair <- apply(BR2,2,max) # Max squared correlation value for each species
 	ind <- apply(BR2,2,which.max) # index of species pair with max correlation
-	sumBIO <- pair*unexp # Biotic variation computation
-	sumUNX <- matrix(1,p,1)-sumENV-sumBIO # Remainder variation is unexplained
-	INDsummary <- cbind(sumENV,sumBIO,sumUNX) # Abiotic, Biotic, Unexplained Variation
+	bio <- pair*unx # Biotic variation computation
+	unx <- matrix(1,p,1)-env-bio # Remainder variation is unexplained
+	IND_summary <- cbind(env,bio,unx) # Abiotic, Biotic, Unexplained Variation
 		
 	# Community variation partitioning
 	ENV <- Avg_Adj_R2 # Environmental variation explained
@@ -33,7 +35,7 @@ if (PRSqYres > 0.05){ # If no significant covariation found
 	ind <- apply(BR2,2,which.max) # index of species pair with max correlation
 	BIO <- sum(pair*unexp*absum)/sum(absum) # Biotic variation computation
 	UNX <- 1-ENV-BIO # Remainder variation is unexplained
-	COMsummary = cbind(ENV, BIO, UNX)
+	COM_summary <- cbind(ENV, BIO, UNX)
 }
-return(list(INDsummary,COMsummary))
+return(list('IND_summary' = IND_summary,'COM_summary' = COM_summary))
 }
